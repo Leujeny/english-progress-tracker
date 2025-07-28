04d659874ce8d90d699dcbb2ad0ca5f2de0184bd
Leujeny <andyfaussau@gmail.com>
2025-07-28 19:32:35 +0200

    About page

---
+++ app/(footer)/about/page.tsx
@@ -0,0 +1,25 @@
+export default function About() {
+
+  return (
+
+    <>
+      <div className="card">
+        <div className="card-body">
+          <h1 className="text-5xl">À propos de ce projet</h1>
+        </div>
+      </div>
+      <h2 className="card-title p-3">Objectif</h2>
+      <div className="card">
+        <div className="card-body">
+          <p>Ce projet a été conçu pour m'aider à suivre ma progression en anglais, de manière simple et personnalisée. Il est entièrement stocké dans le navigateur via LocalStorage, ce qui garantit la confidentialité des données.</p>
+        </div>
+      </div>
+      <h2 className="card-title p-3">Tech Stack</h2>
+      <div className="card">
+        <div className="card-body">
+          <p>Le projet est construit avec Next.js, TailwindCSS, DaisyUI, et utilise LocalStorage pour la persistance.</p>
+        </div>
+      </div>
+    </>
+  );
+}
